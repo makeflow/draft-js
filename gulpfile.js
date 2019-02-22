7397b2aab9c4bdc4df8c4a15a2c7427d50cfa384
Boen <1997@boenfu.cn>
2019-02-22 11:33:12 +0800

    Initialize after fork

--- gulpfile.js
+++ gulpfile.js
@@ -245,7 +245,9 @@ gulp.task(
 
 gulp.task(
   'website:static',
-  gulp.series('dist:min', 'css',
+  gulp.series(
+    'dist:min',
+    'css',
     gulp.parallel(function() {
       return gulp
         .src(paths.dist + '/Draft.min.js')
@@ -256,7 +258,7 @@ gulp.task(
         .src(paths.dist + '/Draft.css')
         .pipe(gulp.dest(paths.static + '/css'));
     }),
-  )
+  ),
 );
 
 gulp.task(
@@ -276,7 +278,7 @@ gulp.task(
 gulp.task(
   'dev',
   gulp.series(function() {
-    gulp.watch(paths.src, ['modules', 'dist']);
+    gulp.watch(paths.src, gulp.parallel('modules', 'dist'));
   }),
 );
 
